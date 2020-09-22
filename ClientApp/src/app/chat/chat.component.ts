334c0b46ea45e0f08a5450dd566174f5ad6e4b10
yersson1991 <yerssonyrc@gmail.com>
2020-09-22 13:45:16 -0500

    componente chat creado v:

---
+++ ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject } from '@angular/core';
+
+
+@Component({
+  selector: 'chat-app',
+  templateUrl: "./chat.component.html"
+
+
+})
+
+
+
+export class ChatComponent{
+
+  public nombre = "yerson duro perri estudie ";
+
+
+}
